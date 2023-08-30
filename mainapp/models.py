ce4f94a598ea578efa1763e91eefd792157a3f47
Naincy Verma <130460928+pytho321@users.noreply.github.com>
2023-08-30 10:28:54 +0530

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,10 +1,1 @@
-from django.db import models
-import random
-# Create your models here.
 
-class Country(models.Model):
-    name = models.CharField(max_length=50)
-    capital = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return self.name
